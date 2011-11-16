88848e92f70eb4289cda15c4c633c11fdff853f9
RJ <rjmarsan@gmail.com>
2011-11-15 22:15:36 -0600

    Tools to swap assets easier

---
+++ swapassets.sh
@@ -0,0 +1,4 @@
+cp ./psds/$1/iconHDPI.png ./res/drawable-hdpi/icon.png
+cp ./psds/$1/iconMDPI.png ./res/drawable-mdpi/icon.png
+cp ./psds/$1/loadingscreen2.png ./res/drawable/loadingscreen2.png
+cp ./psds/$1/loadingscreen2smaller.png ./res/drawable/loadingscreen2smaller.png
