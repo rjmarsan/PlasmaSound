14cd83424520de907484da142fc6712a4c6eca46
RJ Marsan <rjmarsan@gmail.com>
2012-03-28 02:38:13 -0500

    Removing some debugging output - and an extra permission

--- src/com/rj/processing/plasmasoundhd/pd/instruments/Parameter.java
+++ src/com/rj/processing/plasmasoundhd/pd/instruments/Parameter.java
@@ -3,7 +3,7 @@ package com.rj.processing.plasmasoundhd.pd.instruments;
 import org.puredata.core.PdBase;
 
 public class Parameter {
-	boolean DEBUGGG = true;
+	boolean DEBUGGG = false;
 	String name;
 	public final static int CONTINUOUS = 0;  //any value from min to max
 	
